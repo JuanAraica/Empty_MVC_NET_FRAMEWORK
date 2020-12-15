4370c411ff87b41ec1703e69dda3a6659444c1af
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2020-12-14 21:20:38 -0600

    Interaccion y navegacion entre modulos funcional

---
+++ Controllers/VistasHtmlController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plantilla.Controllers
+{
+    public class VistasHtmlController : Controller
+    {
+        public ActionResult MainMenu()
+        {
+            return View();
+        }
+        public ActionResult SinLogin()
+        {
+            return View();
+        }
+        public ActionResult ReservForm()
+        {
+            return View();
+        }
+        public ActionResult HomeAdmin()
+        {
+            return View();
+        }
+    }
+}
