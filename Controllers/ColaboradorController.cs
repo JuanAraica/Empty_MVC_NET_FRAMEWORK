b8b9b484d4d428dd2423809057c53b5a0f98a7c8
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2020-12-15 15:24:54 -0600

    Prim Commit

---
+++ Controllers/ColaboradorController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Plantilla.Models;
+
+namespace Plantilla.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        private DB_A6C1FF_HikingGuanacasteEntities db = new DB_A6C1FF_HikingGuanacasteEntities();
+
+        // GET: Colaborador
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Colaborador.ToListAsync());
+        }
+
+        // GET: Colaborador/Details/5
+        public async Task<ActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Colaborador colaborador = await db.Colaborador.FindAsync(id);
+            if (colaborador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaborador);
+        }
+
+        // GET: Colaborador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Colaborador/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idColaborador,CedColaborador,nombre,apellido1,apellido2,idiomas,nacionalidad,telefono,email,licencia,tipoColaborador")] Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Colaborador.Add(colaborador);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(colaborador);
+        }
+
+        // GET: Colaborador/Edit/5
+        public async Task<ActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Colaborador colaborador = await db.Colaborador.FindAsync(id);
+            if (colaborador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaborador);
+        }
+
+        // POST: Colaborador/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idColaborador,CedColaborador,nombre,apellido1,apellido2,idiomas,nacionalidad,telefono,email,licencia,tipoColaborador")] Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(colaborador).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(colaborador);
+        }
+
+        // GET: Colaborador/Delete/5
+        public async Task<ActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Colaborador colaborador = await db.Colaborador.FindAsync(id);
+            if (colaborador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaborador);
+        }
+
+        // POST: Colaborador/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(short id)
+        {
+            Colaborador colaborador = await db.Colaborador.FindAsync(id);
+            db.Colaborador.Remove(colaborador);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
